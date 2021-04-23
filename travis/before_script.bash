076240281936408f9f9ccad61f49d96f9ba04146
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2021-04-23 00:56:41 -0400

    Corrected Db names

--- travis/before_script.bash
+++ travis/before_script.bash
@@ -5,8 +5,8 @@ if [ "${PGVERSION-}" != "" ]
 then
   # The tricky test user, below, has to actually exist so that it can be used in a test
   # of aclitem formatting. It turns out aclitems cannot contain non-existing users/roles.
-  psql -U postgres -c 'create database PrixBanqueDev;'
+  psql -U postgres -c 'create database prix_banque_test;'
   psql -U postgres -c "create user dev SUPERUSER PASSWORD 'dev';"
-  psql -U postgres -d PrixBanqueDev -f ./scripts/create.sql
-  psql -U postgres -d PrixBanqueDev -f ./scripts/populate.sql
+  psql -U postgres -d prix_banque_test -f ./scripts/create.sql
+  psql -U postgres -d prix_banque_test -f ./scripts/populate.sql
 fi
