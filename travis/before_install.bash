986b6b9be2177c39561b7e712c319f2d82c267d2
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2021-04-23 02:22:27 -0400

    yikes

--- travis/before_install.bash
+++ travis/before_install.bash
@@ -10,9 +10,10 @@ then
   sudo apt-get update -qq
   sudo apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::="--force-confnew" install postgresql-$PGVERSION postgresql-server-dev-$PGVERSION postgresql-contrib-$PGVERSION
   sudo chmod 777 /etc/postgresql/$PGVERSION/main/pg_hba.conf
-  echo "local   postgres             postgres                                     trust"    >  /etc/postgresql/$PGVERSION/main/pg_hba.conf
-  echo "local   all             dev                                     password"    >  /etc/postgresql/$PGVERSION/main/pg_hba.conf
-  echo "host    all             dev             127.0.0.1/32            password"    >  /etc/postgresql/$PGVERSION/main/pg_hba.conf
+  echo "local   all             postgres                                trust"       >  /etc/postgresql/$PGVERSION/main/pg_hba.conf
+  echo "local   all             all                                     trust"       >> /etc/postgresql/$PGVERSION/main/pg_hba.conf
+  echo "local   all             dev                                     password"    >>  /etc/postgresql/$PGVERSION/main/pg_hba.conf
+  echo "host    all             dev             127.0.0.1/32            password"    >>  /etc/postgresql/$PGVERSION/main/pg_hba.conf
   sudo chmod 777 /etc/postgresql/$PGVERSION/main/postgresql.conf
   if $(dpkg --compare-versions $PGVERSION ge 9.6) ; then
     echo "wal_level='logical'"     >> /etc/postgresql/$PGVERSION/main/postgresql.conf
