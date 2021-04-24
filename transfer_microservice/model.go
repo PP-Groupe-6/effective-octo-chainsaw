57ef9a453b0a50d21895281848d681e4a68c02e3
fearlessfriedchicken <arhauet86@gmail.com>
2021-04-24 02:36:33 -0400

    fixed db fields in model

--- transfer_microservice/model.go
+++ transfer_microservice/model.go
@@ -2,12 +2,12 @@ package transfer_microservice
 
 type Transfer struct {
 	ID                string  `json:"transfer_id,omitempty" db:"transfer_id"`
-	Type              string  `json:"transfer_type,omitempty"`
-	Amount            float64 `json:"transfer_amount,omitempty"`
-	AccountPayerId    string  `json:"transfer_payer_id,omitempty"`
-	AccountReceiverId string  `json:"transfer_receiver_id,omitempty"`
-	ReceiverQuestion  string  `json:"receiver_question,omitempty"`
-	ReceiverAnswer    string  `json:"receiver_answer,omitempty"`
-	ScheduledDate     string  `json:"scheduled_transfer_date,omitempty"`
-	ExecutedDate      string  `json:"executed_transfer_date,omitempty"`
+	Type              string  `json:"transfer_type,omitempty" db:"transfer_type"`
+	Amount            float64 `json:"transfer_amount,omitempty" db:"transfer_amount"`
+	AccountPayerId    string  `json:"transfer_payer_id,omitempty" db:"account_transfert_payer_id"`
+	AccountReceiverId string  `json:"transfer_receiver_id,omitempty" db:"account_transfert_receiver_id"`
+	ReceiverQuestion  string  `json:"receiver_question,omitempty" db:"receiver_question"`
+	ReceiverAnswer    string  `json:"receiver_answer,omitempty" db:"receiver_answer"`
+	ScheduledDate     string  `json:"scheduled_transfer_date,omitempty" db:"scheduled_transfert_date"`
+	ExecutedDate      string  `json:"executed_transfer_date,omitempty" db:"executed_transfer_date"`
 }
